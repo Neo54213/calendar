0dea30a046f47b8228c7aaf6feb54b3256e9b519
Name <neo54213@mail.ru>
2016-08-19 11:24:11 +0300

    Drawing calendar code was put into a function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 window.onload = function(){
+
+// Days in russian initializing
+var aWeekDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+
+// Data object initializing
+var dateOfCurrentMonth = new Date();
+
 // People object initializing
 var people = [
 	{
@@ -29,58 +36,58 @@ var people = [
 
 // Th drawing function
 function drawTh(aData){
+	var oTable = document.getElementById("calendar");
 	var oTableTr = document.createElement("tr");
 
     for (key in aData) {
-     var oTableTd = document.createElement("td");
+     var oTableTd = document.createElement("th");
      oTableTd.textContent = aData[key];
      oTableTr.appendChild(oTableTd);
     }
     oTable.appendChild(oTableTr);
 }
 
-// Days in russian initializing
-var aWeekDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+// Calendar draw function
+function drawCalendar(){
+	drawTh(aWeekDays);
 
-// Data object initializing
-var dateOfCurrentMonth = new Date();
+	// Getting number of first day of month
+	dateOfCurrentMonth.setDate(1);
+	var firstDayOfCurrentMonth = dateOfCurrentMonth;
+	var numberOfFirstDayOfCurrentMonth = firstDayOfCurrentMonth.getDay();
 
-// Getting number of first day of month
-dateOfCurrentMonth.setDate(1);
-var firstDayOfCurrentMonth = dateOfCurrentMonth;
-var numberOfFirstDayOfCurrentMonth = firstDayOfCurrentMonth.getDay();
-
-document.getElementById("calendar").innerHTML += "\t\t<tr>\n\t\t";
-var numberOfWeek = 1;
-var date = new Date();
-var numberOfLastDayOfCurrentMonth = 32 - new Date(date.getYear(), date.getMonth(), 32).getDate();
-for(var j = 1; j <= numberOfLastDayOfCurrentMonth; ){
-	for(var i = 1; i <= 7; i++){
-		if(((numberOfFirstDayOfCurrentMonth == i) || (i == 7 && numberOfFirstDayOfCurrentMonth == 0)) 
-		  && numberOfWeek==1
-		  && numberOfFirstDayOfCurrentMonth !=1){
-			document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td colspan=\"" + (i - 1) + "\"></td>\n";
-			document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
-			j++;
-		}else{
-			if(i < numberOfFirstDayOfCurrentMonth && numberOfWeek == 1){
-				continue;
-			}
-			document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
-			j++;
-			if(i == 7){
-				document.getElementById("calendar").innerHTML += "\t\t</tr>\n\t\t\t\t<tr>\n";
-				numberOfWeek++;
-			}
-			if(j > numberOfLastDayOfCurrentMonth){
-				break;
+	document.getElementById("calendar").innerHTML += "\t\t<tr>\n\t\t";
+	var numberOfWeek = 1;
+	var date = new Date();
+	var numberOfLastDayOfCurrentMonth = 32 - new Date(date.getYear(), date.getMonth(), 32).getDate();
+	for(var j = 1; j <= numberOfLastDayOfCurrentMonth; ){
+		for(var i = 1; i <= 7; i++){
+			if(((numberOfFirstDayOfCurrentMonth == i) || (i == 7 && numberOfFirstDayOfCurrentMonth == 0)) 
+			  && numberOfWeek==1
+			  && numberOfFirstDayOfCurrentMonth !=1){
+				document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td colspan=\"" + (i - 1) + "\"></td>\n";
+				document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
+				j++;
+			}else{
+				if(i < numberOfFirstDayOfCurrentMonth && numberOfWeek == 1){
+					continue;
+				}
+				document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
+				j++;
+				if(i == 7){
+					document.getElementById("calendar").innerHTML += "\t\t</tr>\n\t\t\t\t<tr>\n";
+					numberOfWeek++;
+				}
+				if(j > numberOfLastDayOfCurrentMonth){
+					break;
+				}
 			}
 		}
 	}
 }
 
-for(var i = 0; i < document.getElementsByTagName("td").length; i++){
-	document.getElementsByTagName("td")[i].onclick = function(){
+// TD click function
+function tdClick(){
 		// Clearing previous data in texarea
 		document.getElementsByTagName("textarea")[0].innerHTML = "";
 		
@@ -100,6 +107,13 @@ for(var i = 0; i < document.getElementsByTagName("td").length; i++){
 			document.getElementsByTagName("textarea")[0].innerHTML = "Именинников нет.";
 		}
 	};
+
+//----- MAIN PART
+
+drawCalendar();
+
+for(var i = 0; i < document.getElementsByTagName("td").length; i++){
+	document.getElementsByTagName("td")[i].onclick = tdClick;
 }
 
 
