27d1cf2a7cd2e322e3b794361391e59399d7b785
Name <neo54213@mail.ru>
2016-08-19 11:45:18 +0300

    Shorted variables' names

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ window.onload = function(){
 var aWeekDays = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 
 // Data object initializing
-var dateOfCurrentMonth = new Date();
+var oMonthDate = new Date();
 
 // Month name array initializing
 var sMonthName = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль",
 				  "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
 
 // People object initializing
-var people = [
+var oPeople = [
 	{
 		lastName: "Курчатов",
 		firstName: "Борис",
@@ -56,33 +56,33 @@ function drawCalendar(){
 	drawTh(aWeekDays);
 
 	// Getting number of first day of month
-	dateOfCurrentMonth.setDate(1);
-	var firstDayOfCurrentMonth = dateOfCurrentMonth;
-	var numberOfFirstDayOfCurrentMonth = firstDayOfCurrentMonth.getDay();
+	oMonthDate.setDate(1);
+	var oFirstDay = oMonthDate;
+	var nFirstDay = oFirstDay.getDay();
 
 	document.getElementById("calendar").innerHTML += "\t\t<tr>\n\t\t";
-	var numberOfWeek = 1;
+	var nWeek = 1;
 	var date = new Date();
-	var numberOfLastDayOfCurrentMonth = 32 - new Date(date.getYear(), date.getMonth(), 32).getDate();
-	for(var j = 1; j <= numberOfLastDayOfCurrentMonth; ){
+	var nLastMonthDay = 32 - new Date(date.getYear(), date.getMonth(), 32).getDate();
+	for(var j = 1; j <= nLastMonthDay; ){
 		for(var i = 1; i <= 7; i++){
-			if(((numberOfFirstDayOfCurrentMonth == i) || (i == 7 && numberOfFirstDayOfCurrentMonth == 0)) 
-			  && numberOfWeek==1
-			  && numberOfFirstDayOfCurrentMonth !=1){
-				document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td colspan=\"" + (i - 1) + "\"></td>\n";
-				document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
+			if(((nFirstDay == i) || (i == 7 && nFirstDay == 0)) 
+			  && nWeek==1
+			  && nFirstDay !=1){
+				document.getElementsByTagName("tr")[nWeek].innerHTML += "\t\t\t<td colspan=\"" + (i - 1) + "\"></td>\n";
+				document.getElementsByTagName("tr")[nWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
 				j++;
 			}else{
-				if(i < numberOfFirstDayOfCurrentMonth && numberOfWeek == 1){
+				if(i < nFirstDay && nWeek == 1){
 					continue;
 				}
-				document.getElementsByTagName("tr")[numberOfWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
+				document.getElementsByTagName("tr")[nWeek].innerHTML += "\t\t\t<td>" + j + "</td>";
 				j++;
 				if(i == 7){
 					document.getElementById("calendar").innerHTML += "\t\t</tr>\n\t\t\t\t<tr>\n";
-					numberOfWeek++;
+					nWeek++;
 				}
-				if(j > numberOfLastDayOfCurrentMonth){
+				if(j > nLastMonthDay){
 					break;
 				}
 			}
@@ -92,25 +92,25 @@ function drawCalendar(){
 
 // TD click function
 function tdClick(){
-		// Clearing previous data in texarea
-		document.getElementsByTagName("textarea")[0].innerHTML = "";
-		
-		// Searching of people and put info into textarea
-		for(var k in people){
-			if(people[k].birthday == this.innerText){
-				document.getElementsByTagName("textarea")[0].innerHTML += "Именинник: " + 
-				people[k].lastName + " " + people[k].firstName + " " + people[k].fathersName + "\n"
-				+ "Дата рождения: " + ((people[k].birthday < 10)? ("0" + people[k].birthday): (people[k].birthday)) + "." + 
-				((dateOfCurrentMonth.getMonth() + 1 < 10)? "0" + (dateOfCurrentMonth.getMonth() + 1): 
-				(dateOfCurrentMonth.getMonth() + 1)) + "\n" + 
-				"Возраст: " + people[k].age + '\n'+
-				"Город: " + people[k].city + "\n\n";
-			}
+	// Clearing previous data in texarea
+	document.getElementsByTagName("textarea")[0].innerHTML = "";
+	
+	// Searching of oPeople and put info into textarea
+	for(var k in oPeople){
+		if(oPeople[k].birthday == this.innerText){
+			document.getElementsByTagName("textarea")[0].innerHTML += "Именинник: " + 
+			oPeople[k].lastName + " " + oPeople[k].firstName + " " + oPeople[k].fathersName + "\n"
+			+ "Дата рождения: " + ((oPeople[k].birthday < 10)? ("0" + oPeople[k].birthday): (oPeople[k].birthday)) + "." + 
+			((oMonthDate.getMonth() + 1 < 10)? "0" + (oMonthDate.getMonth() + 1): 
+			(oMonthDate.getMonth() + 1)) + "\n" + 
+			"Возраст: " + oPeople[k].age + '\n'+
+			"Город: " + oPeople[k].city + "\n\n";
 		}
-		if(document.getElementsByTagName("textarea")[0].innerHTML ==""){
-			document.getElementsByTagName("textarea")[0].innerHTML = "Именинников нет.";
-		}
-	};
+	}
+	if(document.getElementsByTagName("textarea")[0].innerHTML ==""){
+		document.getElementsByTagName("textarea")[0].innerHTML = "Именинников нет.";
+	}
+}
 
 //----- MAIN PART
 
@@ -120,6 +120,6 @@ for(var i = 0; i < document.getElementsByTagName("td").length; i++){
 	document.getElementsByTagName("td")[i].onclick = tdClick;
 }
 
-document.getElementsByTagName("h1")[0].innerText += sMonthName[dateOfCurrentMonth.getMonth()] + ' ' + dateOfCurrentMonth.getFullYear();
-document.getElementsByTagName("title")[0].innerText += sMonthName[dateOfCurrentMonth.getMonth()] + ' ' + dateOfCurrentMonth.getFullYear();
+document.getElementsByTagName("h1")[0].innerText += sMonthName[oMonthDate.getMonth()] + ' ' + oMonthDate.getFullYear();
+document.getElementsByTagName("title")[0].innerText += sMonthName[oMonthDate.getMonth()] + ' ' + oMonthDate.getFullYear();
 };
