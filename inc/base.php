9fe31e0abda37a1bff69f7c0f3755ad3ea0e4ee0
Ruslan Gladchenko <luga10ka@gmail.com>
2017-01-27 19:10:39 +0300

    BootStrap Add

--- inc/base.php
+++ inc/base.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Stas
- * Date: 06.11.2016
- * Time: 13:08
- */
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $id = $_POST['birthday'];
     $sql = "SELECT * FROM people WHERE birthday=?";
--- index.php
+++ index.php
@@ -8,16 +8,43 @@ require_once 'inc/base.php';
 	<title>Календарь</title>
 	<meta charset='utf-8'/>
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
     <script src='js/jquery-3.1.1.js'></script>
+    <script src='js/bootstrap.min.js'></script>
 </head>
 <body>
-	<h1></h1>
-	<table id="calendar">
-
-	</table>
-	<div>
-		<div id="infoAboutPerson"></div>
-	</div>
+	<h1 class="head-title"></h1>
+	<section id="main">
+		<div class="container">
+		<div class="row">
+			<div class="col-md-4 login-form">
+				<form>
+				<label for="exampleInputEmail1">Email адрес</label>
+				  <div class="input-group">
+				    <span class="input-group-addon">@</span>
+				    <input type="text" class="form-control" id="inputGroupSuccess4" aria-describedby="inputGroupSuccess4Status">
+				  </div>
+				  <div class="form-group">
+				    <label for="exampleInputPassword1">Пароль</label>
+				    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				  </div>
+				  <div class="checkbox">
+				    <label>
+				      <input type="checkbox"> Запомнить
+				    </label>
+				  </div>
+				  <button type="submit" class="btn btn-default">Войти</button>
+				</form>
+			</div>
+			<div class="col-md-8 calendar-form">
+				<table id="calendar" class="table"></table>
+					<div>
+						<div id="infoAboutPerson"></div>
+					</div>
+			</div>
+			</div>
+		</div>
+	</section>
 	<!--input type="button" value='Добавить день'/><br/>
 	<input type="button" value='Удалить день'/-->
 	<script src="js/script.js"></script>
